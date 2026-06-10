805353e51152f44d1094f568c7236fbd96637db4
MelGuerreroL <mvguerrero150@gmail.com>
2026-06-10 10:39:21 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+echo "<h1>¡Bienvenido,</h1>";
+echo "<p>Tu servidor Apache está funcionando correctamente.</p>";
+header("Location: /src/vistasdelprograma/login_usuario_be.php");
+exit;
+
+?>
